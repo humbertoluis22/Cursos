47cd5d38df30e52bbc4013809d6d2287a2be2f56
humbertoluis22 <humbertoluis2013@gmail.com>
2024-09-02 22:16:09 -0300

    atualizando projeto

--- Alura/Python/Flask/jogoteca_segunda_parte/views.py
+++ Alura/Python/Flask/jogoteca_segunda_parte/views.py
@@ -14,7 +14,9 @@ def index():
 def novo():
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return redirect(url_for('login', proxima=url_for('novo')))
-    return render_template('novo.html', titulo='Novo Jogo')
+    
+    form = FormularioJogo()
+    return render_template('novo.html', titulo='Novo Jogo',form = form)
 
  
 @app.route('/editar/<int:id>')
@@ -23,8 +25,13 @@ def editar(id):
         return redirect(url_for('login', proxima= url_for('index')))
     
     jogo = Jogos.query.filter_by(id=id).first()
+    form = FormularioJogo()
+    form.nome.data = jogo.nome
+    form.categoria.data = jogo.categoria
+    form.console.data = jogo.console
+
     capa_jogo = recupera_imagem(id=id)
-    return render_template('editar.html',titulo= 'Editando Jogo',jogo= jogo, capa_jogo = capa_jogo)
+    return render_template('editar.html',titulo= 'Editando Jogo',id= id, capa_jogo = capa_jogo,form = form)
 
 
 @app.route('/deletar/<int:id>')
@@ -40,27 +47,36 @@ def deletar(id):
 
 @app.route('/atualizar',methods= ['POST','GET'])
 def atualizar():
-    jogo =Jogos.query.filter_by(id= request.form['id']).first()
-    jogo.nome = request.form['nome']
-    jogo.categoria = request.form['categoria']
-    jogo.console = request.form['console']
+    form = FormularioJogo(request.form)
 
-    db.session.add(jogo)
-    db.session.commit()
+    if form.validate_on_submit():
 
-    arquivo = request.files['arquivo']
-    upload_arquivo = app.config['UPLOAD_PATH']
-    timestamp = time.time()
-    deletar_imagem(jogo.id)
-    arquivo.save(f'{upload_arquivo}/capa{jogo.id}-{timestamp}.jpg')
+        jogo =Jogos.query.filter_by(id= request.form['id']).first()
+        jogo.nome = form.nome.data
+        jogo.categoria = form.categoria.data
+        jogo.console = form.console.data
+
+        db.session.add(jogo)
+        db.session.commit()
+
+        arquivo = request.files['arquivo']
+        upload_arquivo = app.config['UPLOAD_PATH']
+        timestamp = time.time()
+        deletar_imagem(jogo.id)
+        arquivo.save(f'{upload_arquivo}/capa{jogo.id}-{timestamp}.jpg')
 
     return redirect(url_for('index'))
 
 @app.route('/criar', methods=['POST',])
 def criar():
-    nome = request.form['nome']
-    categoria = request.form['categoria']
-    console = request.form['console']
+    form = FormularioJogo(request.form)
+
+    if not form.validate_on_submit():
+        return redirect(url_for('novo'))
+
+    nome = form.nome.data
+    categoria = form.categoria.data
+    console = form.console.data
 
     novo_jogo = Jogos.query.filter_by(nome = nome).first()
 
