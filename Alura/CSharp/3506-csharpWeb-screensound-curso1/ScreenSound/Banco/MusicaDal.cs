2b0bdafa81d61ea07ff6b9f979a79a3ea6931872
humbertoluis22 <humbertoluis2013@gmail.com>
2025-04-29 00:45:16 -0300

    Adicionando conteudo do curso

---
+++ Alura/CSharp/3506-csharpWeb-screensound-curso1/ScreenSound/Banco/MusicaDal.cs
@@ -0,0 +1,49 @@
+﻿using ScreenSound.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Banco
+{
+    internal class MusicaDal:DAL<Musica>
+    {
+        private readonly ScreenSoundConnection _context;
+        public MusicaDal(ScreenSoundConnection context)
+        {
+            _context = context;
+        }
+
+        public override IEnumerable<Musica> Listar()
+        {
+            var musicas = _context.Musicas.ToList();
+            return musicas;
+        }
+
+        public override void Adicionar(Musica musica)
+        {
+            _context.Musicas.Add(musica);
+            _context.SaveChanges();
+        }
+
+
+        public override void Atualizar(Musica musica)
+        {
+            _context.Musicas.Update(musica);
+            _context.SaveChanges();
+        }
+
+        public override void Deletar(Musica musica)
+        {
+            _context.Musicas.Remove(musica);
+            _context.SaveChanges(); 
+        }
+
+        public Musica? RecuperarPeloNome(string nomeMusica)
+        {
+            var musica = _context.Musicas.FirstOrDefault(m => m.Nome == nomeMusica);
+            return musica;
+        }
+    }
+}
